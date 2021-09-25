6bd9c077c88598210ff6f964779fbe8d0e322da8
Pablo Delgado Krämer <private@pablode.com>
2021-09-25 12:55:02 +0200

    shadergen: use class-based approach for shader compilation.
    A DXC-based implementation will be needed in the future.

---
+++ src/shadergen/src/IShaderCompiler.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+namespace sg
+{
+  class IShaderCompiler
+  {
+  public:
+    IShaderCompiler(const std::string& shaderPath);
+
+    virtual ~IShaderCompiler();
+
+  public:
+    virtual bool init() = 0;
+    virtual void release() = 0;
+
+    virtual bool compileHlslToSpv(const std::string& source,
+                                  const std::string& filePath,
+                                  const char* entryPoint,
+                                  uint32_t* spvSize,
+                                  uint32_t** spv) = 0;
+
+  protected:
+    const std::string m_shaderPath;
+  };
+}
